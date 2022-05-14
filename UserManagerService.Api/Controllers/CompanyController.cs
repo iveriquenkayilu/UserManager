4205e88270c98c46dbd6e4ddb70542c84ae85227
Iverique Nkayilu <iverique.mbungu@ozbul.com>
2022-05-14 17:15:34 +0300

    Added authorize for Admin

--- UserManagerService.Api/Controllers/CompanyController.cs
+++ UserManagerService.Api/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using UserManagerService.Services.Interfaces;
@@ -9,6 +10,7 @@ using UserManagerService.Shared.Models.Company;
 
 namespace UserManagerService.Api.Controllers
 {
+    [Authorize(Roles = Roles.ADMIN)]
     [Route("api/companies")]
     [ApiController]
     public class CompanyController : BaseController
